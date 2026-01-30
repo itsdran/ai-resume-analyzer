b5e62c8b084ea03613e8dc8c771a7c60c6145421
drannnf <drflores2020@plm.edu.ph>
2026-01-30 22:05:06 +0800

    sample

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import tailwindcss from '@tailwindcss/vite';
-import { reactRouter } from '@react-router/dev/vite';
-import path from 'path';
-import checker from 'vite-plugin-checker';
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-    tailwindcss(),
-    reactRouter(),
-    checker({ typescript: true }) 
-  ],
+  plugins: [tsconfigPaths(), tailwindcss(), reactRouter()],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, 'app'),
