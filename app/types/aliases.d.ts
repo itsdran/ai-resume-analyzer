3ecd447b6b57282020618ee0c4114bbba7f5d24b
drannnf <drflores2020@plm.edu.ph>
2026-01-30 22:12:54 +0800

    sample

--- app/types/aliases.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '~/*' {
-    import type { ComponentType } from 'react';
-    const content: ComponentType<any>;
-    export default content;
-}
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), tailwindcss(), reactRouter()],
+  plugins: [tsconfigPaths({projects: ['./tsconfig.build.json']}), tailwindcss(), reactRouter()],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, 'app'),
